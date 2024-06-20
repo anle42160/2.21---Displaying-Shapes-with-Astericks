6333fbdf779a21da5771369252148d617d7e8acd
littl <littl@DESKTOP-CU9RTQ8>
2024-06-19 23:33:23 -0500

    Add project files.

---
+++ 2.21 - Displaying Shapes with Astericks/2.21 - Displaying Shapes with Astericks.cpp
@@ -0,0 +1,19 @@
+// 2.21.) Write a program that prints a box, an oval, an arrow and a diamond as follows:
+// Programmer: Anna Le
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print Box         //Print Oval   // Print Arrow // Print Diamond
+    cout << "**********      " << "   ***        " << "    *         " << "    *    " << endl;
+    cout << "*        *      " << " *     *      " << "   ***        " << "   * *   " << endl;
+    cout << "*        *      " << "*       *     " << "  *****       " << "  *   *  " << endl;
+    cout << "*        *      " << "*       *     " << "    *         " << " *     * " << endl;
+    cout << "*        *      " << "*       *     " << "    *         " << "*       *" << endl;
+    cout << "*        *      " << "*       *     " << "    *         " << " *     * " << endl;
+    cout << "*        *      " << "*       *     " << "    *         " << "  *   *  " << endl;
+    cout << "*        *      " << "*       *     " << "    *         " << "   * *   " << endl;
+    cout << "**********      " << "   ***        " << "    *         " << "    *    " << endl;
+}
